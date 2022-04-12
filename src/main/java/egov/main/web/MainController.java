6b773b53a88d17bc291f914d11582a41c8ff249b
liufeier <hwd3004@gmail.com>
2022-04-12 12:55:00 +0900

    table space

--- src/main/java/egov/main/web/MainController.java
+++ src/main/java/egov/main/web/MainController.java
@@ -4,7 +4,10 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
@@ -15,6 +18,40 @@ public class MainController {
 //		ModelMap - 사용자에게 데이터를 전달
 
 		return "main/main";
+	}
+
+	@RequestMapping(value = "/main2.do")
+	public String main2(HttpServletRequest request, ModelMap model) {
+
+		return "main/main2";
+	}
+
+//	@RequestMapping(value = "/main3.do", method = RequestMethod.POST)
+	@RequestMapping(value = "/main3.do")
+	public String main3(@RequestParam("pw") String pw, HttpServletRequest request, ModelMap model) {
+
+		int userNo = Integer.parseInt(request.getParameter("userNo").toString());
+
+		String id = request.getParameter("id").toString();
+
+		if (id.equals("asd")) {
+			model.addAttribute("userid", id);
+		} else {
+			model.addAttribute("userid", "잘못된 입력");
+		}
+
+		userNo += 5;
+
+		model.addAttribute("userNo", userNo);
+
+		return "main/main3";
+	}
+
+	@RequestMapping(value = "/main4/{userNo}.do")
+	public String main4(@PathVariable String userNo, HttpServletRequest request, ModelMap model) {
+		model.addAttribute("userNo", userNo);
+
+		return "main/main3";
 	}
 
 }
---
+++ src/main/webapp/WEB-INF/jsp/main/main2.jsp
@@ -0,0 +1,22 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<h1>main2.do</h1>
+
+	<form action="main3.do" method="post">
+		<input type="hidden" name="userNo" value="10">
+		<!--  -->
+		<input type="text" name="id">
+		<!--  -->
+		<input type="password" name="pwd">
+		<!--  -->
+		<input type="submit">
+	</form>
+</body>
+</html>
